52009ee803e74d9c1d90dd7c0c0d3b20e64555fd
camiiruiz <103294966+camiiruiz@users.noreply.github.com>
2023-02-23 15:02:45 -0300

    finalizacion de filtro

--- src/imageProcessing.js
+++ src/imageProcessing.js
@@ -5,44 +5,29 @@ export function processImage(img) {
   const newImg = new cv.Mat();
   let image2 =original.clone();
 
-  // let rect = new cv.Rect(0, 0, 70, 70);
-  // image2=image2.roi(rect);
-
   cv.cvtColor(image2, newImg, cv.COLOR_RGB2HSV,0);
  
   let channels = new cv.MatVector();
   cv.split(newImg,channels);
   let H = channels.get(0);
-  // // let prom = new cv.Mat();
-  // // let dev = new cv.Mat();
-  // let res = new cv.mean(chred);
-  // let mask = new cv.Mat();
 
-  let accumulate = false;
-  let channelz = [0];
-  let histSize = [256];
-  let ranges = [0, 180];
-  let hist = new cv.Mat();
-  let mask = new cv.Mat();
-
-  // You can try more different parameters
-  //cv.calcHist(H, channelz, mask, hist, histSize, ranges, accumulate);
-
-  let result = cv.minMaxLoc(H);
-
-  console.log(result);
-
-  let lowerb = cv.matFromArray(1, 3, CV_8UC1, [14, 0, 0]);
-  let upperb = cv.matFromArray(1, 3, CV_8UC1, [25, 255, 255]);
+  let lowerb = cv.matFromArray(1, 3, CV_8UC1, [12, 0, 0]);
+  let upperb = cv.matFromArray(1, 3, CV_8UC1, [22, 255, 255]);
 
   cv.inRange(newImg, lowerb, upperb, newImg);
-  // cv.multiply(newImg,image2,newImg);
-  // let M = cv.Mat.ones(6, 6, cv.CV_8U);
-  // cv.morphologyEx(newImg,newImg, cv.MORPH_OPEN, M);
+
+  let M = cv.Mat.ones(5, 5, cv.CV_8U);
+  cv.morphologyEx(newImg,newImg, cv.MORPH_ERODE, M);
+
+  let contours = new cv.MatVector();
+  let hierarchy = new cv.Mat();
+  cv.findContours(newImg, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+  console.log(contours);
+  cv.drawContours(image2,contours,-1,[0,0,0,1],4);
 
   return {
     image: image2,
-    croppedImage: newImg,
+    croppedImage: original,
     //detectedPixels: result,
     detectedPixels: cv.countNonZero(newImg),
   };
